9a95e648addee7e9e58381e03c9e882ddc9629e1
Hakan Şahin <hakansahin993@gmail.com>
2016-02-29 01:51:20 +0200

    Stanford POS Tagger Integration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,36 @@
+import Models.Document;
 import Models.Ontology;
 import Utils.Parser;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by kerata on 28/02/16.
  */
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        Ontology ontology = Parser.buildOntology("/Users/kerata/IdeaProjects/bionlp/src/main/resources/OntoBiotope_BioNLP-ST-2016.obo");
-        System.out.println(ontology.toString());
+    public static String DATA_PATH = "src/main/resources/data";
+    public static Document[] documents;
+
+    public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
+        Ontology ontology = Parser.buildOntology("src/main/resources/OntoBiotope_BioNLP-ST-2016.obo");
+//        System.out.println(ontology.toString());
+
+        // Iterates over given files and constructs document objects.
+        File[] listOfFiles = (new File(DATA_PATH)).listFiles();
+        documents = new Document[listOfFiles.length];
+        for(int i=0; i<listOfFiles.length; i++){
+            File file = listOfFiles[i];
+            if(!file.getName().endsWith(".txt")) continue;
+            documents[i] = new Document(new String(Files.readAllBytes(Paths.get(file.getPath())), StandardCharsets.UTF_8));
+        }
+
+        // Tags a document as an example and prints the tagged text line by line.
+        documents[0].posTagger();
+        documents[0].printTaggedDocument();
     }
 }
---
+++ src/main/java/Models/Document.java
@@ -0,0 +1,30 @@
+package Models;
+
+import Utils.POSTagger;
+import edu.stanford.nlp.ling.Sentence;
+
+import java.util.List;
+
+/**
+ * Created by hakansahin on 28/02/16.
+ */
+public class Document {
+
+    private String text;
+    private List taggedText;
+    public Document(String text){
+        this.text = text;
+    }
+
+    public List posTagger(){
+        return this.taggedText = POSTagger.tagDocument(this.text);
+    }
+
+    public void printTaggedDocument(){
+        for(Object sentence : this.taggedText)
+            System.out.println(Sentence.listToString((List)sentence, false));
+    }
+
+    public String getText(){ return this.text; }
+    public List getTaggedText(){ return this.taggedText; }
+}
